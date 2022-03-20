c88d83d8a2962af6402b643d3df2c800b1b4cee8
mayke_s <74941370+maykesolucuines@users.noreply.github.com>
2022-03-19 19:28:59 -0500

    Update vozatexto.js

--- cbi_luces/html/scripts/vozatexto.js
+++ cbi_luces/html/scripts/vozatexto.js
@@ -1,32 +1,64 @@
 const contenido = document.querySelector('.contenido')
-const btnGrabarTexto = document.querySelector('.btn-grabar')
+var mensaje;
 
-
-/* metodo que se ejecuta al empezar a grabar */
-reconocimiento.onstart = ()=>{
-    contenido.innerHTML = 'cual es tu peticion...'
+function iniciar_reco_VOZ(){
+  document.getElementById("btn").style.display = "none" 
+  aranque();
 }
 
 
-/* Metodo que se ejecuta al terminar la grabación */
-reconocimiento.onresult = event =>{
-    let mensaje = event.results[0][0].transcript
-    contenido.innerHTML = mensaje
+function aranque() {
+
+  // Create a Speech Recognition object with callback
+  Reconocimiento = new p5.SpeechRec('es-CO', voz_escuchada);
+  // This must come after setting the properties
+  Reconocimiento.start();
+
+  // Speech recognized event
+  function voz_escuchada() {
+    
+    if (Reconocimiento.resultValue) {
+      mensaje = Reconocimiento.resultString;
+      // Show user
+      texto_salida.html(mensaje);
+      console.log(mensaje);
+    }
     leerTextoCondicionado(mensaje)
+  }
+  //////////////////////////////////////////////////////////////////////////////////////
+Reconocimiento.onError = function(event) {
+console.log(event.error);
+  setTimeout(() => { 
+    contenido.innerHTML= 'Error: ' + event.error; 
+    Reconocimiento.start();   
+      console.log("Error");     
+      },1500); 
 }
-/* Función que se lanza para locutar lo grabado 
-const leerTextoSimple = (mensaje)=>{
-    const voz = new SpeechSynthesisUtterance()
-    voz.text = mensaje
-    window.speechSynthesis.speak(voz)
-}    
-*/
+
+  //////////////////////////////////////////////////////////////////////////////////////
+  Reconocimiento.onStart = function(event) {
+    setTimeout(() => { 
+        contenido.innerHTML = 'cual es tu peticion...' 
+        console.log("sonido inicio");     
+        },1000); 
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////
+  Reconocimiento.onEnd = function(event) {
+    setTimeout(() => {
+      console.log("sonido Termidado");
+      Reconocimiento.start();     
+        },800); 
+  }
+
+}
+
 
  /*Función que condiciona la respuesta dependiendo de el contenido de la grabación */
 const leerTextoCondicionado = (mensaje)=>{
-    const voz = new SpeechSynthesisUtterance()
+    //const voz = new SpeechSynthesisUtterance()
     if(mensaje.includes('lámpara 1 prender') || mensaje.includes('lámpara 1 encender') || mensaje.includes('encender lámpara uno') || mensaje.includes('encender lámpara 1')){
-        voz.text = 'Listo!!!'
+        //voz.text = 'Listo!!!'
         var channel = ably.channels.get(topic_raiz+"/actions/sw1");
         channel.publish(clientId, "1"); 
         console.log('Mensaje sw1 on');
@@ -35,7 +67,7 @@ const leerTextoCondicionado = (mensaje)=>{
     }
 
     if(mensaje.includes('lámpara 1 Apagar') || mensaje.includes('lámpara 1 apagar') || mensaje.includes('Apagar lámpara uno') || mensaje.includes('Apagar lámpara 1')){
-        voz.text = 'Listo!!!'
+        //voz.text = 'Listo!!!'
         var channel = ably.channels.get(topic_raiz+"/actions/sw1");
         channel.publish(clientId, "0"); 
         console.log('Mensaje sw1 off');
@@ -44,7 +76,7 @@ const leerTextoCondicionado = (mensaje)=>{
     }
 ///////////////////////////////////
 if(mensaje.includes('lámpara 2 prender') || mensaje.includes('lámpara 2 encender') || mensaje.includes('encender lámpara dos')|| mensaje.includes('encender lámpara 2')){
-    voz.text = 'Listo!!!'
+    //voz.text = 'Listo!!!'
     var channel = ably.channels.get(topic_raiz+"/actions/sw2");
     channel.publish(clientId, "1"); 
     console.log('Mensaje sw2 on');
@@ -53,7 +85,7 @@ if(mensaje.includes('lámpara 2 prender') || mensaje.includes('lámpara 2 encend
 }
 
 if(mensaje.includes('lámpara 2 Apagar') || mensaje.includes('lámpara 2 apagar') || mensaje.includes('Apagar lámpara dos')|| mensaje.includes('Apagar lámpara 2')){
-    voz.text = 'Listo!!!'
+    //voz.text = 'Listo!!!'
     var channel = ably.channels.get(topic_raiz+"/actions/sw2");
     channel.publish(clientId, "0"); 
     console.log('Mensaje sw2 off');
@@ -62,7 +94,7 @@ if(mensaje.includes('lámpara 2 Apagar') || mensaje.includes('lámpara 2 apagar'
 }
 ///////////////////////////////////
 if(mensaje.includes('lámpara 3 prender') || mensaje.includes('lámpara 3 encender') || mensaje.includes('encender lámpara tres')|| mensaje.includes('encender lámpara 3')){
-    voz.text = 'Listo!!!'
+    //voz.text = 'Listo!!!'
     var channel = ably.channels.get(topic_raiz+"/actions/sw3");
     channel.publish(clientId, "1"); 
     console.log('Mensaje sw3 on');
@@ -71,7 +103,7 @@ if(mensaje.includes('lámpara 3 prender') || mensaje.includes('lámpara 3 encend
 }
 
 if(mensaje.includes('lámpara 3 Apagar') || mensaje.includes('lámpara 3 apagar') || mensaje.includes('Apagar lámpara tres')|| mensaje.includes('Apagar lámpara 3')){
-    voz.text = 'Listo!!!'
+    //voz.text = 'Listo!!!'
     var channel = ably.channels.get(topic_raiz+"/actions/sw3");
     channel.publish(clientId, "0"); 
     console.log('Mensaje sw3 off');
@@ -80,7 +112,7 @@ if(mensaje.includes('lámpara 3 Apagar') || mensaje.includes('lámpara 3 apagar'
 }
 ///////////////////////////////////
 if(mensaje.includes('lámpara 4 prender') || mensaje.includes('lámpara 4 encender') || mensaje.includes('encender lámpara cuatro')|| mensaje.includes('encender lámpara 4')){
-    voz.text = 'Listo!!!'
+    //voz.text = 'Listo!!!'
     var channel = ably.channels.get(topic_raiz+"/actions/sw4");
     channel.publish(clientId, "1"); 
     console.log('Mensaje sw4 on');
@@ -89,7 +121,7 @@ if(mensaje.includes('lámpara 4 prender') || mensaje.includes('lámpara 4 encend
 }
 
 if(mensaje.includes('lámpara 4 Apagar') || mensaje.includes('lámpara 4 a pagar') || mensaje.includes('Apagar lámpara cuatro')|| mensaje.includes('Apagar lámpara 4')){
-    voz.text = 'Listo!!!'
+    //voz.text = 'Listo!!!'
     var channel = ably.channels.get(topic_raiz+"/actions/sw4");
     channel.publish(clientId, "0"); 
     console.log('Mensaje sw4 off');
@@ -98,7 +130,7 @@ if(mensaje.includes('lámpara 4 Apagar') || mensaje.includes('lámpara 4 a pagar
 }
 ///////////////////////////////////
 if(mensaje.includes('lámpara 5 prender') || mensaje.includes('lámpara 5 encender') || mensaje.includes('encender lámpara cinco')|| mensaje.includes('encender lámpara 5')){
-    voz.text = 'Listo!!!'
+   // voz.text = 'Listo!!!'
     var channel = ably.channels.get(topic_raiz+"/actions/sw5");
     channel.publish(clientId, "1"); 
     console.log('Mensaje sw5 on');
@@ -106,7 +138,7 @@ if(mensaje.includes('lámpara 5 prender') || mensaje.includes('lámpara 5 encend
     //voz.text = mensaje
 }
 if(mensaje.includes('lámpara 5 Apagar') || mensaje.includes('lámpara 5 apagar') || mensaje.includes('Apagar lámpara cinco')|| mensaje.includes('Apagar lámpara 5')){
-    voz.text = 'Listo!!!'
+    //voz.text = 'Listo!!!'
     var channel = ably.channels.get(topic_raiz+"/actions/sw5");
     channel.publish(clientId, "0"); 
     console.log('Mensaje sw5 off');
@@ -115,7 +147,7 @@ if(mensaje.includes('lámpara 5 Apagar') || mensaje.includes('lámpara 5 apagar'
 }
 ///////////////////////////////////
 if(mensaje.includes('lámpara 6 prender') || mensaje.includes('lámpara 6 encender') || mensaje.includes('encender lámpara seis') || mensaje.includes('encender lámpara 6')){
-    voz.text = 'Listo!!!'
+    //voz.text = 'Listo!!!'
     var channel = ably.channels.get(topic_raiz+"/actions/sw6");
     channel.publish(clientId, "1"); 
     console.log('Mensaje sw6 on');
@@ -124,7 +156,7 @@ if(mensaje.includes('lámpara 6 prender') || mensaje.includes('lámpara 6 encend
 }
 
 if(mensaje.includes('lámpara 6 Apagar') || mensaje.includes('lámpara 6 apagar') || mensaje.includes('Apagar lámpara seis') || mensaje.includes('Apagar lámpara 6')){
-    voz.text = 'Listo!!!'
+   // voz.text = 'Listo!!!'
     var channel = ably.channels.get(topic_raiz+"/actions/sw6");
     channel.publish(clientId, "0"); 
     console.log('Mensaje sw6 off');
@@ -133,7 +165,7 @@ if(mensaje.includes('lámpara 6 Apagar') || mensaje.includes('lámpara 6 apagar'
 }
 ///////////////////////////////////
 if(mensaje.includes('lámpara 7 prender') || mensaje.includes('lámpara 7 encender') || mensaje.includes('encender lámpara siete')|| mensaje.includes('encender lámpara 7')){
-voz.text = 'Listo!!!'
+//voz.text = 'Listo!!!'
 var channel = ably.channels.get(topic_raiz+"/actions/sw7");
 channel.publish(clientId, "1"); 
 console.log('Mensaje sw2 on');
@@ -142,7 +174,7 @@ console.log('Mensaje sw2 on');
 }
 
 if(mensaje.includes('lámpara 7 Apagar') || mensaje.includes('lámpara 7 apagar') || mensaje.includes('Apagar lámpara siete')|| mensaje.includes('Apagar lámpara 7')){
-voz.text = 'Listo!!!'
+//voz.text = 'Listo!!!'
 var channel = ably.channels.get(topic_raiz+"/actions/sw7");
 channel.publish(clientId, "0"); 
 console.log('Mensaje sw7 off');
@@ -151,7 +183,7 @@ console.log('Mensaje sw7 off');
 }
 ///////////////////////////////////
 if(mensaje.includes('lámpara 8 prender') || mensaje.includes('lámpara 8 encender') || mensaje.includes('encender lámpara ocho')|| mensaje.includes('encender lámpara 8')){
-voz.text = 'Listo!!!'
+//voz.text = 'Listo!!!'
 var channel = ably.channels.get(topic_raiz+"/actions/sw8");
 channel.publish(clientId, "1"); 
 console.log('Mensaje sw8 on');
@@ -160,7 +192,7 @@ console.log('Mensaje sw8 on');
 }
 
 if(mensaje.includes('lámpara 8 Apagar') || mensaje.includes('lámpara 8 apagar') || mensaje.includes('Apagar lámpara ocho')|| mensaje.includes('Apagar lámpara 8')){
-voz.text = 'Listo!!!'
+//voz.text = 'Listo!!!'
 var channel = ably.channels.get(topic_raiz+"/actions/sw8");
 channel.publish(clientId, "0"); 
 console.log('Mensaje sw3 off');
@@ -169,7 +201,7 @@ console.log('Mensaje sw3 off');
 }
 ///////////////////////////////////
 if(mensaje.includes('lámpara 9 prender') || mensaje.includes('lámpara 9 encender') || mensaje.includes('encender lámpara nueve')|| mensaje.includes('encender lámpara 9')){
-voz.text = 'Listo!!!'
+//voz.text = 'Listo!!!'
 var channel = ably.channels.get(topic_raiz+"/actions/sw9");
 channel.publish(clientId, "1"); 
 console.log('Mensaje sw9 on');
@@ -178,7 +210,7 @@ console.log('Mensaje sw9 on');
 }
 
 if(mensaje.includes('lámpara 9 Apagar') || mensaje.includes('lámpara 9 apagar') || mensaje.includes('Apagar lámpara nueve')|| mensaje.includes('Apagar lámpara 9')){
-voz.text = 'Listo!!!'
+//voz.text = 'Listo!!!'
 var channel = ably.channels.get(topic_raiz+"/actions/sw9");
 channel.publish(clientId, "0"); 
 console.log('Mensaje sw9 off');
@@ -187,7 +219,7 @@ console.log('Mensaje sw9 off');
 }
 ///////////////////////////////////
 if(mensaje.includes('lámpara 10 prender') || mensaje.includes('lámpara 10 encender') || mensaje.includes('encender lámpara diez')|| mensaje.includes('encender lámpara 10')){
-voz.text = 'Listo!!!'
+//voz.text = 'Listo!!!'
 var channel = ably.channels.get(topic_raiz+"/actions/sw10");
 channel.publish(clientId, "1"); 
 console.log('Mensaje sw10 on');
@@ -196,7 +228,7 @@ console.log('Mensaje sw10 on');
 }
 
 if(mensaje.includes('lámpara 10 Apagar') || mensaje.includes('lámpara 10 a pagar') || mensaje.includes('Apagar lámpara diez')|| mensaje.includes('Apagar lámpara 10')){
-voz.text = 'Listo!!!'
+//voz.text = 'Listo!!!'
 var channel = ably.channels.get(topic_raiz+"/actions/sw10");
 channel.publish(clientId, "0"); 
 console.log('Mensaje sw10 off');
@@ -206,7 +238,7 @@ console.log('Mensaje sw10 off');
 ///////////////////////////////////
 /////////// Encender todas las lamparas
 if(mensaje.includes('encender todas las lámparas') || mensaje.includes('encender todas las luces')){
-voz.text = 'Listo!!!'
+//voz.text = 'Listo!!!'
 
 setTimeout(() => {
 var channel = ably.channels.get(topic_raiz+"/actions/sw1");
@@ -273,7 +305,7 @@ console.log('Mensaje sw10 on');
 }
 /////////// Apagar todas las lamparas
 if(mensaje.includes('Apagar todas las lámparas')|| mensaje.includes('Apagar todas las luces')){
-    voz.text = 'Listo!!!'
+    //voz.text = 'Listo!!!'
     
     setTimeout(() => {
     var channel = ably.channels.get(topic_raiz+"/actions/sw1");
@@ -338,17 +370,16 @@ if(mensaje.includes('Apagar todas las lámparas')|| mensaje.includes('Apagar tod
     }else{
     //voz.text = mensaje
     }
-
-    voz.rate = 0.9; // velocidad de reproduccion valor menor mas lento
-    window.speechSynthesis.speak(voz)
+    
+if(mensaje.includes('apágate') || mensaje.includes('reiníciate') || mensaje.includes('apaga te')){
+  console.log("apagando");
+  location.reload();
+}else{
+//voz.text = mensaje
 }
 
-
-
-
-/* Evento para empezar a grabar pulsado el boton */
-btnGrabarTexto.addEventListener('click', ()=>{
-    reconocimiento.start()
-})
+   // voz.rate = 0.9; // velocidad de reproduccion valor menor mas lento
+   // window.speechSynthesis.speak(voz)
+}
 
 
